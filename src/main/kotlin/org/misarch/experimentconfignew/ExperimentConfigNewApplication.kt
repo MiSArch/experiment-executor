6203ab8283dbff7cfc01d51714493915f019023a
Elias Müller <elias_benedikt.mueller@porsche.de>
2025-03-21 14:30:11 +0100

    chore: inital commit

---
+++ src/main/kotlin/org/misarch/experimentconfignew/ExperimentConfigNewApplication.kt
@@ -0,0 +1,11 @@
+package org.misarch.experimentconfignew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExperimentConfigNewApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExperimentConfigNewApplication>(*args)
+}
---
+++ src/test/kotlin/org/misarch/experimentconfignew/ExperimentConfigNewApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.misarch.experimentconfignew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExperimentConfigNewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
