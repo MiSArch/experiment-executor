52aa206346effb828edec8b16c11205708d4645d
Elias Müller <elias_benedikt.mueller@porsche.de>
2025-04-14 11:12:38 +0200

    feat: use docker for chaos toolkit

--- src/main/kotlin/org/misarch/experimentexecutor/plugin/failure/chaostoolkit/ChaosToolkitPlugin.kt
+++ src/main/kotlin/org/misarch/experimentexecutor/plugin/failure/chaostoolkit/ChaosToolkitPlugin.kt
@@ -2,23 +2,20 @@ package org.misarch.experimentexecutor.plugin.failure.chaostoolkit
 
 import org.misarch.experimentexecutor.executor.model.Failure
 import org.misarch.experimentexecutor.plugin.failure.FailurePluginInterface
-import java.io.File
 
-class ChaosToolkitPlugin: FailurePluginInterface {
+class ChaosToolkitPlugin : FailurePluginInterface {
     override suspend fun executeFailure(failure: Failure): Boolean {
         return runCatching {
-            val currentDir = File(".").absolutePath
-            val venv = "~/.venvs/chaostk/bin/activate"
-
-            val process = ProcessBuilder("bash", "-c",
-                "source $venv && PYTHONPATH=\"$currentDir\" chaos run ${failure.chaosToolkit!!.pathUri}")
-                .directory(File("."))
-                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            val filePath = failure.chaosToolkit!!.pathUri
+            val process = ProcessBuilder(
+                "bash", "-c",
+                "docker run --rm -v $filePath:/app/experiment.yaml -v /var/run/docker.sock:/var/run/docker.sock custom-chaostoolkit"
+            ).redirectOutput(ProcessBuilder.Redirect.INHERIT)
                 .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .start()
 
-            val exitCode = process.waitFor()
-            exitCode == 0
+            process.waitFor()
+            true
         }.getOrElse { e ->
             e.printStackTrace()
             false
